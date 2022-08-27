0dcedbb3432bc28983037774878e65039b6cdb05
thejullius <julio-goncalves@outlook.com>
2022-08-27 10:56:53 -0300

    update order form

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,8 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   accepts_nested_attributes_for :order_items
 
-  validates :name, :email, :phone, :zip_code, :state, :city, :address, :address_number, :document, presence: true
+  validates :name, :email, :phone, :zip_code, :state, :shipment_service,
+            :city, :address, :address_number, :document, presence: true
 
   enum status: { awaiting_review: 0, reviewed: 1, denied: 2, posted: 3 }
 end
